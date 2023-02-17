25bfe5ce0934ab24ba1c55b64d563adeeb95a1cc
minoverse <121744450+minoverse@users.noreply.github.com>
2023-02-17 17:18:35 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/lv2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,10 +1,9 @@
-
 SELECT
 CASE 
 WHEN (0 < PRICE) AND (PRICE < 10000) then 0
              WHEN (10000 <= PRICE) and (PRICE < 20000) then 10000
              WHEN (20000 <= PRICE) and (PRICE < 30000) then 20000
-             WHEN (30000 <= PRICE) and (PRICE < 40000) then 30000
+             WHEN (30000 <= PRICE) and (PRICE < 40000) then '30000'
              WHEN (40000 <= PRICE) and (PRICE < 50000) then 40000
              WHEN (50000 <= PRICE) and (PRICE < 60000) then 50000
              WHEN (60000 <= PRICE) and (PRICE < 70000) then 60000
